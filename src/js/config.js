e082aa601589e0624784f024d042804bb7b2bfeb
Zer0-07 <abasit.999@gmail.com>
2023-03-17 09:48:09 +0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e8c3f237-44a4-451a-8d8d-f687f2fde700';
+export const MODAL_CLOSE_SEC = 2.5;
